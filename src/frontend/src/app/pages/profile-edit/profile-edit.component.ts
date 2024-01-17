391dd8c9d0c38a2f6c1d8b94e88c6b4df0f6467f
MiaArnix <108235271+MiaArnix@users.noreply.github.com>
2024-01-17 20:44:38 +0100

    Update src/frontend/src/app/pages/profile-edit/profile-edit.component.ts
    Co-authored-by: Nathan Mc Grath <contact@nathanos.dev>

--- src/frontend/src/app/pages/profile-edit/profile-edit.component.ts
+++ src/frontend/src/app/pages/profile-edit/profile-edit.component.ts
@@ -117,7 +117,7 @@ import { SOCIAL_MEDIA_INPUTS } from './profile.model';
             <button
               type="submit"
               [title]="profileForm.invalid ? 'Fix the validation errors' : undefined"
-              [disabled]="!profileForm.valid"
+              [disabled]="profileForm.invalid"
               class="text-white bg-blue-500 py-1 px-4 rounded text-lg enabled:hover:bg-blue-600 disabled:bg-blue-300"
             >
               Save
