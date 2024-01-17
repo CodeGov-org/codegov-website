6355d41de61880d92c115b92c69c945bbe986e5a
NathanosDev <contact@nathanos.dev>
2024-01-18 00:42:37 +0100

    refactor: use date-fns for date formatting

--- src/backend/integration/src/support/date.ts
+++ src/backend/integration/src/support/date.ts
@@ -1,30 +1,5 @@
-function pad(num: number): string {
-  return num < 10 ? '0' + String(num) : String(num);
-}
+import { format } from 'date-fns';
 
 export function dateToRfc3339(date: Date): string {
-  const offset = -date.getTimezoneOffset();
-  const sign = offset >= 0 ? '+' : '-';
-
-  const absOffset = Math.abs(offset);
-  const padOffset = absOffset < 600 ? '0' : '';
-  const offsetHours = Math.floor(absOffset / 60);
-
-  const offsetMinutes = pad(absOffset % 60);
-  const offsetString = `${sign}${padOffset}${offsetHours}:${offsetMinutes}`;
-
-  return (
-    date.getFullYear() +
-    '-' +
-    pad(date.getMonth() + 1) +
-    '-' +
-    pad(date.getDate()) +
-    'T' +
-    pad(date.getHours()) +
-    ':' +
-    pad(date.getMinutes()) +
-    ':' +
-    pad(date.getSeconds()) +
-    offsetString
-  );
+  return format(date, "yyyy-MM-dd'T'HH:mm:ssxxx");
 }
