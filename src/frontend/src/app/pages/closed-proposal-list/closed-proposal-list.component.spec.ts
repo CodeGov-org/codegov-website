466ac313074b643cd9282339b2d70025ad1d730d
Magdalena Kanicka <magdalena.kanicka@gmail.com>
2024-03-01 23:55:39 +0100

    test: fix to test setup

--- src/frontend/src/app/pages/closed-proposal-list/closed-proposal-list.component.spec.ts
+++ src/frontend/src/app/pages/closed-proposal-list/closed-proposal-list.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { ProposalService } from '~core/state';
+import {
+  ProposalServiceMock,
+  proposalServiceMockFactory,
+} from '~core/state/proposal/proposal.service.mock';
 import { ClosedProposalListComponent } from './closed-proposal-list.component';
 
 describe('ClosedProposalListComponent', () => {
   let component: ClosedProposalListComponent;
   let fixture: ComponentFixture<ClosedProposalListComponent>;
+  let proposalServiceMock: ProposalServiceMock;
 
   beforeEach(async () => {
+    proposalServiceMock = proposalServiceMockFactory();
+    proposalServiceMock.openProposalList$ = of([]);
+
     await TestBed.configureTestingModule({
-      imports: [ClosedProposalListComponent]
-    })
-    .compileComponents();
-    
+      imports: [ClosedProposalListComponent],
+      providers: [{ provide: ProposalService, useValue: proposalServiceMock }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ClosedProposalListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
