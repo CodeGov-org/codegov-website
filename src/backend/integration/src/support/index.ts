b67679d68d8c18c273e401f357b4ad939abfd2ca
ilbertt <luca.berte98@hotmail.it>
2024-04-28 11:52:31 +0200

    test: remove helpers

--- src/backend/integration/src/support/index.ts
+++ src/backend/integration/src/support/index.ts
@@ -1,7 +1,6 @@
 export * from './common';
 export * from './date';
 export * from './governance';
-export * from './helpers';
 export * from './identity';
 export * from './response';
 export * from './wasm';
