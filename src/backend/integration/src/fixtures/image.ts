35b3648d5f62f9ff344eccc1eb361b5d7faa32fc
ilbertt <luca.berte98@hotmail.it>
2024-04-28 13:57:07 +0200

    style: prettier

--- src/backend/integration/src/fixtures/image.ts
+++ src/backend/integration/src/fixtures/image.ts
@@ -1,9 +1,6 @@
-import { readFileSync } from "fs";
-import { resolve } from "path";
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
-const CODEGOV_LOGO_PATH = resolve(
-  __dirname,
-  'codegov-logo.png',
-);
+const CODEGOV_LOGO_PATH = resolve(__dirname, 'codegov-logo.png');
 
 export const CODEGOV_LOGO_PNG = new Uint8Array(readFileSync(CODEGOV_LOGO_PATH));
