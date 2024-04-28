a4ed4daa7e7250d8b642df91d3d8c35cdfb96f46
ilbertt <luca.berte98@hotmail.it>
2024-04-28 11:41:20 +0200

    test: unneeded files

--- scripts/run-integration-tests-2.sh
+++
@@ -1,8 +0,0 @@
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "update proposal review commit batch three"
-
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "delete proposal review commit batch one"
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "delete proposal review commit batch two"
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "update proposal review commit batch three"
-
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "delete proposal review commit batch one"
-pnpm -F backend-integration test -- proposal_review_commit.spec -t "delete proposal review commit batch two"
